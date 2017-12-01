49c2f35cd6a223a289fe7a2c299be02f11629d46
Matthew James Davis <davismj@foursails.co>
2017-12-01 18:40:35 +0900

    Feature: Add globals view engine hooks

---
+++ src/globals-view-engine-hooks.js
@@ -0,0 +1,7 @@
+export class JavaScriptConstantsViewEngineHooks {
+  beforeBind(view) {
+    for (let global of ['Array','Object','JSON','Date','Math','Number','RegExp','Reflect']) {
+      view.overrideContext[global] = window[global];
+    }
+  }
+}
