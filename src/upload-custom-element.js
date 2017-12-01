b978ea9fc29033aad6f7ac5462bab97f94d2ca47
Matthew James Davis <davismj@foursails.co>
2017-12-01 17:44:25 +0900

    Feature: Add upload custom element

---
+++ src/upload-custom-element.js
@@ -0,0 +1,30 @@
+import { bindingMode } from 'aurelia-binding';
+import { containerless, bindable } from 'aurelia-templating';
+
+@containerless
+export class UploadCustomElement {
+  
+  @bindable class;
+  @bindable style;
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) file;
+  
+  upload;
+  
+  onFileSelected = (event) => {
+    this.file = event.target.files[0];
+    event.target.value = null;
+  }
+  
+  constructor() {
+    this.upload = document.createElement('input');
+    this.upload.type = 'file';
+  }
+  
+  bind() {
+    this.upload.addEventListener('change', this.onFileSelected);
+  }
+  
+  unbind() {
+    this.upload.removeEventListener('change', this.onFileSelected);
+  }
+}
