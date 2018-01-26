7090fe641649d849ac10081d170dc8a5770e234d
Matthew James Davis <davismj@foursails.co>
2018-01-27 02:20:56 +0900

    fix(index): Remove outdated component

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import { PLATFORM } from 'aurelia-pal';
 
 export function configure(aurelia) {
   aurelia.globalResources([
-    PLATFORM.moduleName('./refresh-binding-behavior'),
-    PLATFORM.moduleName('./type-custom-attribute'),
-    PLATFORM.moduleName('./upload-custom-element'),
-    PLATFORM.moduleName('./globals-view-engine-hooks'),
     PLATFORM.moduleName('./date-time-value-converter'),
     PLATFORM.moduleName('./filter-value-converter'),
+    PLATFORM.moduleName('./globals-view-engine-hooks'),
     PLATFORM.moduleName('./json-value-converter'),
     PLATFORM.moduleName('./number-value-converter'),
-    PLATFORM.moduleName('./sort-value-converter')
+    PLATFORM.moduleName('./refresh-binding-behavior'),
+    PLATFORM.moduleName('./split-value-converter'),
+    PLATFORM.moduleName('./type-custom-attribute'),
+    PLATFORM.moduleName('./upload-custom-element'),
   ]);
 }
